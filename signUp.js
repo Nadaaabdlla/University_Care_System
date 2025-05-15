e6b371e8e85f0aa3b29bafdbaf9e1860fe8a4d0f
nada <nadaaabdlla@gmail.com>
2025-05-15 19:47:06 +0100

    خليت الصفحة متناسبة لجميع الاجهزة وعدلت الشكل و خليت كود الجافا سكريبت منظم اكتر

--- signUp.js
+++ signUp.js
@@ -1,18 +1,19 @@
 window.addEventListener("load", () => {
-    document.getElementById("formTypeBtn1").addEventListener("click", () => {
-        document.getElementById("drId").classList.remove("hidden");
-        document.getElementById("formTypeBtn2").classList.remove("hover");
-        document.getElementById("formTypeBtn1").classList.add("hover");
-        document.getElementById("formTypeBtn1").addEventListener("click", function (click) {
-            click.preventDefault();
-        });
+    const formTypeBtn1 = document.querySelector("#formTypeContainer button:first-child");
+    const formTypeBtn2 = document.querySelector("#formTypeContainer button:last-child");
+    const drId = document.getElementById("drId");
+
+    formTypeBtn1.addEventListener("click", (e) => {
+        e.preventDefault();
+        drId.classList.remove("hidden");
+        formTypeBtn2.classList.remove("hover");
+        formTypeBtn1.classList.add("hover");
     });
-    document.getElementById("formTypeBtn2").addEventListener("click", () => {
-        document.getElementById("drId").classList.add("hidden");
-        document.getElementById("formTypeBtn1").classList.remove("hover");
-        document.getElementById("formTypeBtn2").classList.add("hover");
-        document.getElementById("formTypeBtn2").addEventListener("click", function (click) {
-            click.preventDefault();
-        });
+
+    formTypeBtn2.addEventListener("click", (e) => {
+        e.preventDefault();
+        drId.classList.add("hidden");
+        formTypeBtn1.classList.remove("hover");
+        formTypeBtn2.classList.add("hover");
     });
-})
+});
